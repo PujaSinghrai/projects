e2a8233cb411ae28e941fcbc2f163422f86aff56
PujaSinghrai <80784144+PujaSinghrai@users.noreply.github.com>
2021-04-01 11:00:46 +0530

    loop

---
+++ one.js
@@ -0,0 +1,50 @@
+
+// writing a program to check number is divisible by 7 or not
+
+
+let x=70;
+
+if(x%7==0){
+    console.log("x is divisible by 7");
+}else{
+    console.log("x is not divisible by 7");
+}
+
+
+// // writing a program to check number is multible by 3 or not
+
+let a =30;
+
+if(a % 3 == 0){
+    console.log("a is a multiple of 3");
+}else{
+    console.log("a is not amultiple of 3");
+}
+
+// // //WAP to check a number is possitive or not?
+
+let b = 2;
+    
+
+    if (b=>0) {
+        console.log("b number is possitiv");
+    } else {
+        console.log("b munber is  not possitive ");
+    }
+
+
+
+///// wap to check munber is 3 digit  number or not ?
+
+
+
+    let number = 123;
+    let newNumber = number.toString();
+    newNumber.length == 3 ? console.log("3 digit number") : console.log("not 3 digit number")
+   
+   
+    ///// WAP  to check a munber is having 4 at unit's place or not?
+    
+    let munber=2345
+     let X = munber.toString()
+     X.slice(x.length-1)==4 ? console.log("units place  is 4"): console.log("unit place is not 4") ;
